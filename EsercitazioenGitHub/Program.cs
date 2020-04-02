ddebada2d6f678c99e1b7d0438d02b92c189d0b3
labinfo <labinfo4@secondarieclago.it>
2020-04-02 09:27:54 +0200

    Aggiungere i file di progetto.

---
+++ EsercitazioenGitHub/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EsercitazioenGitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
